1e4b77b28865458296e3e80f9a804e01ed998ae1
Jake Zatecky <jake.zatecky@gmail.com>
2016-07-16 01:18:57 -0400

    Simplify gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,25 @@
-var gulp     = require('gulp');
-var scsslint = require('gulp-scss-lint');
-var sass     = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const scsslint = require('gulp-scss-lint');
+const autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('test-style', function () {
-	return gulp.src(['./scss/**/*.scss', '!./scss/_cayman.scss'])
-		.pipe(scsslint());
-});
+gulp.task('test-style', () =>
+	gulp.src(['./scss/**/*.scss', '!./scss/_cayman.scss'])
+		.pipe(scsslint())
+);
 
-gulp.task('build-style', ['test-style'], function () {
-	return gulp.src('./scss/**/*.scss')
+gulp.task('build-style', ['test-style'], () =>
+	gulp.src('./scss/**/*.scss')
 		.pipe(sass({
 			outputStyle: 'expanded',
 		}).on('error', sass.logError))
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions']
 		}))
-		.pipe(gulp.dest('./css'));
-});
+		.pipe(gulp.dest('./css'))
+);
 
-gulp.task('watch', function () {
+gulp.task('watch', () => {
 	gulp.watch(['./scss/**/*.scss'], ['build-style']);
 });
 
